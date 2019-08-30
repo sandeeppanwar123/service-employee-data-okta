abe038969906211ea3ad49817d7f93461e2f6cc6
sksingh <sandeep.net.java@gmail.com>
2019-08-30 15:40:55 +0530

    Improvements

---
+++ src/main/java/com/daffodil/resources/UrlConstants.java
@@ -0,0 +1,16 @@
+package com.daffodil.resources;
+
+public class UrlConstants {
+    private UrlConstants() {
+    }
+
+    public static final String VERSION = "v1";
+
+    public static final String API = "employees";
+
+    public static final String VERSIONED_API = API+VERSION;
+
+    public static final String EMP_ID = "{empId}";
+
+
+}
